553db7f89b2463855a89928b3df86821194317d7
docker.hiragi <docker@hiragi.com>
2019-07-15 06:04:28 +0000

    A: Node construction and build asm

--- 9hcc.c
+++ 9hcc.c
@@ -20,11 +20,36 @@ struct Token {
   char *str;
 };
 
+typedef enum {
+	      ND_ADD,
+	      ND_SUB,
+	      ND_MUL,
+	      ND_DIV,
+	      ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+// prototypes
+Node *expr();
+Node *mul();
+Node *term();
+
+  
+
 // token variable
 Token *token;
 
 // user input
 char *user_input;
+
 
 // notify error pos
 void error_at(char *loc, char *fmt, ...) {
@@ -106,7 +131,7 @@ Token *tokenize(char *p) {
       continue;
     }
     // + or -
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -125,35 +150,114 @@ Token *tokenize(char *p) {
 }
 
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *term() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node *mul() {
+  Node *node = term();
+
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, term());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, term());
+    } else {
+      return node;
+    }
+  }
+}
+
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  mul rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+} 
+
+
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Invalid argument count");
+    fprintf(stderr, "Invalid argument count\n");
     return 1;
   }
   // user input save
   user_input = argv[1];
+  // tokenize
   token = tokenize(argv[1]);
-
+  // parse
+  Node *node = expr();
+  
   // asm header
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // check if first is num
-  printf("  mov rax, %d\n", expect_number());
+  // generate code from tree
+  gen(node);
 
-  // consume tokens
-  // output asm
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // store result to rax
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
