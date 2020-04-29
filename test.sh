94c064f2dc0cfca196f1a060219e11fb052c0356
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-04-29 13:17:25 +0900

    Update: test semicolon

--- test.sh
+++ test.sh
@@ -4,10 +4,13 @@ try() {
     expected="$1"
     input="$2"
 
-    src/9hcc "$input" > tmp.s
+    bin/9hcc "$input" > ./tmp.s
     gcc -o tmp tmp.s
     ./tmp
     actual="$?"
+
+    rm tmp.s
+    rm tmp
 
     if [ "$actual" = "$expected" ]; then
 	echo "$input => $actual"
@@ -18,19 +21,19 @@ try() {
 }
 
 
-try 0 0
-try 42 42
-try 23 '20+4-1'
-try 128 '10 + 114 - 23 + 27'
-try 47 "5+6*7"
-try 15 "5*(9-6)"
-try 4 "(3+5)/2"
-try 10 "-(3+3)*5+40"
-try 1 "1==1" #mean true
-try 0 "1!=1" #mean false
-try 1 "1<=1" 
-try 0 "1<1"
-try 0 "a=1"
+try 0 '0;'
+try 42 '42;'
+try 23 '20+4-1;'
+try 128 '10 + 114 - 23 + 27;'
+try 47 "5+6*7;"
+try 15 "5*(9-6);"
+try 4 "(3+5)/2;"
+try 10 "-(3+3)*5+40;"
+try 1 "1==1;" #mean true
+try 0 "1!=1;" #mean false
+try 1 "1<=1;" 
+try 0 "1<1;"
+try 10 "a=10;"
 
 echo OK
 
